019ce4484cc1db140a7416a4fe4fcc499e624536
eugenefdf <eugenefdf@gmail.com>
2024-10-19 12:47:54 +0800

    c

--- streamlit_app_methodology.py
+++ streamlit_app_methodology.py
@@ -7,4 +7,4 @@ def display_methodology():
     """)
     
     # Add an image
-    st.image("https://raw.githubusercontent.com/eugenefdf/EAS-Learning-Roadmap/main/Project%20Flow%20Chart%20(v2).jpg, caption="Flow Chart", use_column_width=True)
+    st.image("https://raw.githubusercontent.com/eugenefdf/EAS-Learning-Roadmap/main/Project%20Flow%20Chart%20(v2).jpg", caption="Flow Chart", use_column_width=True)
